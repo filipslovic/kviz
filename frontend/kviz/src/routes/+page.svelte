6ee84108ab93f98115d2279e9010ec80d069c98f
filipslovic <ppera9000@gmail.com>
2024-04-23 23:05:52 +0200

    promenjen izgled polja za unos(forme), sklonjen border sa polja i ostavljena samo donja linija, pomerene margine, promenjen font

--- frontend/kviz/src/routes/+page.svelte
+++ frontend/kviz/src/routes/+page.svelte
@@ -15,18 +15,18 @@
     <div class="container-fluid">
         <form class="mx-auto">
             <h4 class="text-center">Login</h4>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <div class="mt-4">
+              <label for="exampleInputEmail1 " class="form-label">Email address</label>
               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               
             </div>
-            <div class="mb-3 mt-5">
+            <div class="mb-3 mt-4">
               <label for="exampleInputPassword1" class="form-label">Password</label>
               <input type="password" class="form-control" id="exampleInputPassword1">
               <div id="emailHelp" class="form-text">Nemate nalog? <a href="#"> registrujte se!</a></div>
             </div>
             
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-primary mt-4">Login</button>
           </form>
     </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
