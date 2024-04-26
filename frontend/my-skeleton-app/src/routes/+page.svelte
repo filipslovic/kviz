9c8436e684fd9cc25f33411ec62fe3d428294e56
nikola.j <nikolajovanovic940@gmail.com>
2024-04-26 13:32:42 +0200

    dodat hover na linkove iz zaglavlja

--- frontend/my-skeleton-app/src/routes/+page.svelte
+++ frontend/my-skeleton-app/src/routes/+page.svelte
@@ -1,16 +1,16 @@
-<header class="bg-white shadow-md">
+<header class="bg-white shadow-md relative">
     <div class="container mx-auto px-4">
         <nav class="flex justify-between items-center py-4">
             <div class="flex items-center">
                 <a href="+page.svelte"><img src="/catLogotip.png" alt="Logo" class="h-16 w-auto mr-4"></a>
-                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Play Game</a>
-                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Create Question</a>
-                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Status</a>
-                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Records</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Play Game</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Create Question</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Status</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Records</a>
             </div>
             <div class="flex items-center">
-                <a href="login" class="mr-4 text-2xl font-bold text-black">Login</a>
-                <a href="+page.svelte" class="text-2xl font-bold text-black">Register</a>
+                <a href="login" class="mr-4 text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Login</a>
+                <a href="+page.svelte" class="text-2xl font-bold text-black transition-colors duration-300 hover:text-blue-500">Register</a>
             </div>
         </nav>
     </div>
