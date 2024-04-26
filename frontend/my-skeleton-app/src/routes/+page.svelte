efb96672dadf49995bd06145d8c7e872a28f4cf7
nikola.j <nikolajovanovic940@gmail.com>
2024-04-26 11:59:51 +0200

    pocetna verzija home page sa headerom gotova, stilizovano

--- frontend/my-skeleton-app/src/routes/+page.svelte
+++ frontend/my-skeleton-app/src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<h1>dobrodosli</h1>
+
+    <header class="bg-white shadow-md">
+        <div class="container mx-auto px-4">
+            <nav class="flex justify-between items-center py-4">
+                <div class="flex items-center">
+                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Home Page</a>
+                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Play Game</a>
+                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Create Question</a>
+                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Status</a>
+                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Records</a>
+                </div>
+                <div class="flex items-center">
+                    <a href="login" class="mr-4 text-2xl font-bold text-black">Login</a>
+                    <a href="+page.svelte" class="text-2xl font-bold text-black">Register</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-red-200 via-orange-200 to-yellow-200">
+
+</div>
