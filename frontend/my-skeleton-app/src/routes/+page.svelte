e53551a528f478f48a9e2980f7eef9bf89c44d60
nikola.j <nikolajovanovic940@gmail.com>
2024-04-26 13:12:50 +0200

    napravljen logo i ubacen kao link umesto home paragrafa, dodata slika logoa na home page, sredjeno zaglavlje

--- frontend/my-skeleton-app/src/routes/+page.svelte
+++ frontend/my-skeleton-app/src/routes/+page.svelte
@@ -1,21 +1,20 @@
-
-    <header class="bg-white shadow-md">
-        <div class="container mx-auto px-4">
-            <nav class="flex justify-between items-center py-4">
-                <div class="flex items-center">
-                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Home Page</a>
-                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Play Game</a>
-                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Create Question</a>
-                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Status</a>
-                    <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Records</a>
-                </div>
-                <div class="flex items-center">
-                    <a href="login" class="mr-4 text-2xl font-bold text-black">Login</a>
-                    <a href="+page.svelte" class="text-2xl font-bold text-black">Register</a>
-                </div>
-            </nav>
-        </div>
-    </header>
-    <div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-red-200 via-orange-200 to-yellow-200">
-
+<header class="bg-white shadow-md">
+    <div class="container mx-auto px-4">
+        <nav class="flex justify-between items-center py-4">
+            <div class="flex items-center">
+                <a href="+page.svelte"><img src="/catLogotip.png" alt="Logo" class="h-16 w-auto mr-4"></a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Play Game</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Create Question</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Status</a>
+                <a href="+page.svelte" class="mr-4 text-2xl font-bold text-black">Records</a>
+            </div>
+            <div class="flex items-center">
+                <a href="login" class="mr-4 text-2xl font-bold text-black">Login</a>
+                <a href="+page.svelte" class="text-2xl font-bold text-black">Register</a>
+            </div>
+        </nav>
+    </div>
+</header>
+<div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-red-200 via-orange-200 to-yellow-200">
+    <img src="/catLogotip1.png" alt="">
 </div>
