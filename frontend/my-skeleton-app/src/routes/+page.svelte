0549a0ded3a19586af2155d3e59a6a4b995d0122
nikola.j <nikolajovanovic940@gmail.com>
2024-04-26 10:52:54 +0200

    pozadina promenjena, uvecana login forma, stilizovano

--- frontend/my-skeleton-app/src/routes/+page.svelte
+++ frontend/my-skeleton-app/src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<div class="min-h-screen flex justify-center items-center bg-gray-100">
-    <form class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h4 class="text-center text-xl font-bold mb-4 text-black">Login</h4>
-        <div class="mb-4">
-            <label for="exampleInputEmail1" class="block text-gray-700 text-sm font-bold mb-2">Email address</label>
-            <input type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-6">
-            <label for="exampleInputPassword1" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="exampleInputPassword1">
-            <p class="text-gray-600 text-xs italic">Nemate nalog? <a href="register" class="text-blue-500">registrujte se!</a></p>
-        </div>
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
-    </form>
+<div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-red-200 via-orange-200 to-yellow-200">
+    <div class="absolute inset-0 bg-black bg-opacity-0 backdrop-filter backdrop-blur-sm flex justify-center items-center">
+        <form class="bg-transparent border bg-white shadow-md rounded-xl px-10 py-12 mb-8">
+            <h4 class="text-center text-3xl font-bold mb-6 text-black">Login</h4>
+            <div class="mb-6">
+                <label for="exampleInputEmail1" class="block text-gray-700 text-lg font-bold mb-2">Email address</label>
+                <input type="email" class="shadow appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-6">
+                <label for="exampleInputPassword1" class="block text-gray-700 text-lg font-bold mb-2">Password</label>
+                <input type="password" class="shadow appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="exampleInputPassword1">
+                <p class="text-gray-600 text-sm italic">Nemate nalog? <a href="register" class="text-blue-500">registrujte se!</a></p>
+            </div>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline">Login</button>
+        </form>
+    </div>
 </div>
