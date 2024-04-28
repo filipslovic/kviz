086d8ffb3137b7d64643c3a951b791d42153fdf9
filipslovic <ppera9000@gmail.com>
2024-04-28 22:26:23 +0200

    pozicionirana login forma i doradjen login button. Koriscen postcss

--- frontend/my-skeleton-app/src/routes/login/+page.svelte
+++ frontend/my-skeleton-app/src/routes/login/+page.svelte
@@ -22,7 +22,7 @@
 
     <div class="min-h-screen flex justify-center items-center">
         <div class="absolute inset-0 bg-black bg-opacity-0 backdrop-filter backdrop-blur-sm flex justify-center items-center">
-            <form class="bg-transparent border bg-white shadow-md rounded-xl px-10 py-12 mb-8">
+            <form class="login bg-transparent border bg-white shadow-md rounded-xl px-10 py-12 mb-8">
                 <h4 class="text-center text-3xl font-bold mb-6 text-black">Login</h4>
                 <div class="mb-6">
                     <label for="exampleInputEmail1" class="block text-gray-700 text-lg font-bold mb-2">Email address</label>
@@ -33,7 +33,9 @@
                     <input type="password" class="shadow appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="exampleInputPassword1">
                     <p class="text-gray-600 text-sm italic">Nemate nalog? <a href="register" class="text-blue-500">registrujte se!</a></p>
                 </div>
-                <button type="submit" class="bg-primary-siva hover:bg-blue-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline">Login</button>
+                <div style="text-align: center;">
+                    <button type="submit" class="btn  bg-primary-siva transition-transform duration-200 hover:bg-gradient-to-br from-red-200 via-orange-200 to-yellow-200 text-white font-bold py-3 px-8 rounded-full focus:outline-none focus:shadow-outline hover:text-primary-siva ">Login</button>
+                </div>
             </form>
         </div>
     </div>
